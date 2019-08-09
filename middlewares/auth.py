535ca05d2143f11fc2751b135a6617df00af896d
Sargis <s.aghabekyan@yandex.ru>
2019-08-10 00:51:29 +0400

    init

---
+++ middlewares/auth.py
@@ -0,0 +1,17 @@
+class SimpleMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        print(1111111)
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        response = self.get_response(request)
+        print(222222222)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
