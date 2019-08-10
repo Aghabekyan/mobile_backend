d6ad46076fe88eaafd8597469caf4af6649ba403
Sargis <s.aghabekyan@yandex.ru>
2019-08-10 11:26:44 +0400

    added firebase keys

--- middlewares/auth.py
+++ middlewares/auth.py
@@ -21,10 +21,31 @@ class SimpleMiddleware:
 from users.models import User
 from rest_framework import authentication
 from rest_framework import exceptions
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import auth
 
 class ExampleAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         print(3333333)
+
+        # cred = credentials.Certificate("key.json")
+        cred = credentials.Certificate("./key.json")
+        firebase_admin.initialize_app(cred)
+
+        # user = auth.create_user(
+        #     email='sssssekyan@yandex.ru',
+        #     email_verified=False,
+        #     password='secretPassword',
+        #     display_name='John Doe',
+        #     photo_url='http://www.example.com/12345678/photo.png',
+        #     disabled=False)
+
+        # print('Sucessfully created new user: {0}'.format(user.uid))
+
+        decoded_token = auth.verify_id_token('eyJhbGciOiJSUzI1NiIsImtpZCI6IjcyODRlYTZiNGZlZDBmZDc1MzE4NTg2NDZmZDYzNjE1ZGQ3YTIyZjUiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoibmFyZWsiLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vdGVzdGFwcC1hNzg1OSIsImF1ZCI6InRlc3RhcHAtYTc4NTkiLCJhdXRoX3RpbWUiOjE1NjUyNzAxNDEsInVzZXJfaWQiOiJPZUc4NFY3a0hZZVZoYzIxOGJQNmpUV0FsVGwyIiwic3ViIjoiT2VHODRWN2tIWWVWaGMyMThiUDZqVFdBbFRsMiIsImlhdCI6MTU2NTI3MDE0MSwiZXhwIjoxNTY1MjczNzQxLCJlbWFpbCI6Im5hcmVrc2ltb255YW45NEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsibmFyZWtzaW1vbnlhbjk0QGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.gdWJsra57XjPlmtQoX69QbcmzSmRTYRNxKcvJQ5owu9k1sM4iSgZ_WFoKZiqm3qrboDUmkGPVMk_mDEhMz9xuvE6pGQEQojJkLOAz3EeSgzV-xRKE_zkgLcHOXY5l74xP2AI7bWcH7DxrwwirKa4_h8etKN61Nso4ucikzGox2WAJpLOFmYpnXGKsCALe0iFKNAW9W4PIuAyIWUve0t78LrZ-5Pq28mJza5WnBuRQ7aQybd2DyHUrlzPZ-zFL3kkwoXkaFVFfqibpgcVmhYqSGgpiJ7MdatwrGGK4zKyEftyN0WkP1A1DGYfIPahO8VdrWhiTyxTzYjEZlO3aDnWjA')
+        # uid = decoded_token["uid"]
+
         username = request.META.get('X_USERNAME') # get the username request header
         print(username)
         if not username: # no username passed in request headers
