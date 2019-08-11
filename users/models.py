4d8ef365f0e33218b939bc439c69e2da9cbf9b22
Sargis <s.aghabekyan@yandex.ru>
2019-08-11 14:16:17 +0400

    added Role model

--- users/models.py
+++ users/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 
+class Role(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+
 class User(models.Model):
     id = models.AutoField(primary_key=True)
     uid = models.CharField(max_length=200, unique=True)
     name = models.CharField(max_length=200)
+    role = models.ForeignKey(Role, null=True, blank=True, on_delete=models.SET_NULL)
