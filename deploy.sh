ea63af29cca672a5c7e239b5fe48ba44b2f1b076
Sargis <s.aghabekyan@yandex.ru>
2019-08-22 17:45:12 +0400

    minor change

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-docker build -t aghabekyan374/mobile-backend:latest -t aghabekyan374/mobile-backend:$SHA -f ./Dockerfile
+docker build -t aghabekyan374/mobile-backend:latest -t aghabekyan374/mobile-backend:$SHA -f ./Dockerfile .
 
 docker push aghabekyan374/mobile-backend:latest
 
