43678e1758d0939ffef15e731adc5f3a4c43adbd
Sargis <s.aghabekyan@yandex.ru>
2019-08-22 17:53:09 +0400

    test

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,6 @@ docker build -t aghabekyan374/mobile-backend:latest -t aghabekyan374/mobile-back
 docker push aghabekyan374/mobile-backend:latest
 
 docker push aghabekyan374/mobile-backend:$SHA
+
+echo '12345678' | ssh corda@10.0.3.151 --password-stdin
+ls
