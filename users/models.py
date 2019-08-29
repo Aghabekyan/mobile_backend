e14ee5e16cd013d322c07c559f71f623dfd4bea2
Sargis <s.aghabekyan@yandex.ru>
2019-08-29 17:14:31 +0400

    added reset password mkdel

--- users/models.py
+++ users/models.py
@@ -13,3 +13,9 @@ class User(models.Model):
 
     def is_admin(self):
         return self.role.id == 1
+
+
+class RezetPassword(models.Model):
+    uid = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
