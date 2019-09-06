fa596554e964a82e995dd63555d3228d6d3f8fdb
Sargis <s.aghabekyan@yandex.ru>
2019-09-06 18:03:34 +0400

    changed FCMToken modle

--- main/models.py
+++ main/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 
+class DeviceType(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'main_device_type'
+
+
 class FCMToken(models.Model):
     uid = models.CharField(max_length=200, primary_key=True)
     token = models.CharField(max_length=200)
+    device_id = models.CharField(max_length=200)
+    device_type = models.ForeignKey(DeviceType, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'main_fcm_token'
--- main/serializers.py
+++ main/serializers.py
@@ -5,7 +5,9 @@ from rest_framework import serializers
 class FCMTokenSerializer(serializers.ModelSerializer):
     uid = serializers.CharField(required=True)
     token = serializers.CharField(required=False)
+    device_id = serializers.CharField(required=False)
+    device_type = serializers.CharField(required=False)
 
     class Meta:
         model = FCMToken
-        fields = ['uid', 'token']
+        fields = ['uid', 'token', 'device_id', 'device_type']
